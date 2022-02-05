2c90e001ea8c0b18af12d31c926ae00263d2ac4b
neonPebble <akashparhi0@gmail.com>
2022-02-05 23:58:58 +0530

    better comment

--- config/database.js
+++ config/database.js
@@ -7,7 +7,7 @@ require("dotenv").config();
  */
 
 /**
- * Connect to MongoDB Server using the connection string in the `.env` file.  To implement this, place the following
+ * Connect to MongoDB Server using the connection string in the `.env` file.  To implement this, place a string similar to the following
  * string into the `.env` file
  *
  * DB_STRING=mongodb://<user>:<password>@localhost:27017/database_name
