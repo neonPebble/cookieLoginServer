aa81451a6f07efcbb637d54b9ca0595bf6dd3d68
neonPebble <akashparhi0@gmail.com>
2022-02-13 06:08:38 +0530

    url spelling correction

--- index.js
+++ index.js
@@ -124,20 +124,22 @@ app.get("/localntlist", (req, res, next) => {
 
 //the static files with their directories  were put in  ".output/static"  directory
 
-app.use(
+/*app.use(
   "/public/listdista",
   express.static(path.join(__dirname, ".output/static/public/listdista"))
-);
+);*/
+
 app.get("/notelist", isAuth, (req, res, next) => {
   res.sendFile(path.join(__dirname, "vercelpages/listdista", "index.html"));
 });
 
 // remove these  last routes when you are done
 
-app.use(
+/*app.use(
   "/public/loclistdist",
   express.static(path.join(__dirname, ".output/static/public/loclistdist"))
 );
+*/
 
 app.get("/localntlist", (req, res, next) => {
   res.sendFile(path.join(__dirname, "vercelpages/loclistdist", "index.html"));
